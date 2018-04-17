1732930e0e985f7659e67fc670f75c6787ecea48
brie_mcnally <brienne.mcnally@gmail.com>
2018-04-17 10:26:48 -0700

    Refactor

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './lib/game.js',
+  output:{
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  }
+};
